000b7e498b725f9abb48c4eecf3dbe8c38cf98ba
sunilunnava <sunil.unnava@gmail.com>
2017-11-28 17:35:34 -0500

    changes to deploy.sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -85,7 +85,7 @@ log "================================================================"
 
 
 # configmap
-${KUBECTL} apply  --namespace ${APP_NS} --context ${K8S_CTX} ${KUBECTL_OPTS} -f ${SCRIPT_DIR}/.${PREFIX}-cfgmap.yml
+${KUBECTL} apply  --namespace ${APP_NS}  ${KUBECTL_OPTS} -f ${SCRIPT_DIR}/.${PREFIX}-cfgmap.yml
 
 # svc
 ${KUBECTL} apply \
